ee7d7114cbc1ed8c8bcdf42efcf900d3baa0fb6b
Comf2 <otischaka4@gmail.com>
2023-01-17 23:58:54 -0500

    Finished Currency Converter

--- currency-converter/Form1.Designer.cs
+++ currency-converter/Form1.Designer.cs
@@ -29,11 +29,330 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.dollarResult = new System.Windows.Forms.TextBox();
+            this.poundsResult = new System.Windows.Forms.TextBox();
+            this.eurosResult = new System.Windows.Forms.TextBox();
+            this.yenResult = new System.Windows.Forms.TextBox();
+            this.dollarsLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.hundredAmmount = new System.Windows.Forms.TextBox();
+            this.fiftyAmount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.twentyAmount = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tenAmount = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fiveAmount = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.oneAmount = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dollarsRadio = new System.Windows.Forms.RadioButton();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.poundsRadio = new System.Windows.Forms.RadioButton();
+            this.eurosRadio = new System.Windows.Forms.RadioButton();
+            this.yenRadio = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // convertButton
+            // 
+            this.convertButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.convertButton.Location = new System.Drawing.Point(362, 371);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(75, 23);
+            this.convertButton.TabIndex = 0;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = false;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // dollarResult
+            // 
+            this.dollarResult.Location = new System.Drawing.Point(122, 89);
+            this.dollarResult.Name = "dollarResult";
+            this.dollarResult.Size = new System.Drawing.Size(100, 20);
+            this.dollarResult.TabIndex = 1;
+            this.dollarResult.TextChanged += new System.EventHandler(this.dollarResult_TextChanged);
+            // 
+            // poundsResult
+            // 
+            this.poundsResult.Location = new System.Drawing.Point(122, 129);
+            this.poundsResult.Name = "poundsResult";
+            this.poundsResult.Size = new System.Drawing.Size(100, 20);
+            this.poundsResult.TabIndex = 2;
+            // 
+            // eurosResult
+            // 
+            this.eurosResult.Location = new System.Drawing.Point(122, 174);
+            this.eurosResult.Name = "eurosResult";
+            this.eurosResult.Size = new System.Drawing.Size(100, 20);
+            this.eurosResult.TabIndex = 3;
+            // 
+            // yenResult
+            // 
+            this.yenResult.Location = new System.Drawing.Point(122, 219);
+            this.yenResult.Name = "yenResult";
+            this.yenResult.Size = new System.Drawing.Size(100, 20);
+            this.yenResult.TabIndex = 4;
+            // 
+            // dollarsLabel
+            // 
+            this.dollarsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.dollarsLabel.Location = new System.Drawing.Point(228, 89);
+            this.dollarsLabel.Name = "dollarsLabel";
+            this.dollarsLabel.Size = new System.Drawing.Size(100, 23);
+            this.dollarsLabel.TabIndex = 5;
+            this.dollarsLabel.Text = "Dollars ";
+            this.dollarsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(616, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 23);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "$100";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // hundredAmmount
+            // 
+            this.hundredAmmount.Location = new System.Drawing.Point(540, 92);
+            this.hundredAmmount.Name = "hundredAmmount";
+            this.hundredAmmount.Size = new System.Drawing.Size(70, 20);
+            this.hundredAmmount.TabIndex = 10;
+            // 
+            // fiftyAmount
+            // 
+            this.fiftyAmount.Location = new System.Drawing.Point(540, 118);
+            this.fiftyAmount.Name = "fiftyAmount";
+            this.fiftyAmount.Size = new System.Drawing.Size(70, 20);
+            this.fiftyAmount.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(616, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 23);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "$50";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // twentyAmount
+            // 
+            this.twentyAmount.Location = new System.Drawing.Point(540, 144);
+            this.twentyAmount.Name = "twentyAmount";
+            this.twentyAmount.Size = new System.Drawing.Size(70, 20);
+            this.twentyAmount.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(616, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 23);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "$20";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tenAmount
+            // 
+            this.tenAmount.Location = new System.Drawing.Point(540, 170);
+            this.tenAmount.Name = "tenAmount";
+            this.tenAmount.Size = new System.Drawing.Size(70, 20);
+            this.tenAmount.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(616, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 23);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "$10";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // fiveAmount
+            // 
+            this.fiveAmount.Location = new System.Drawing.Point(540, 196);
+            this.fiveAmount.Name = "fiveAmount";
+            this.fiveAmount.Size = new System.Drawing.Size(70, 20);
+            this.fiveAmount.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(616, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 23);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "$5";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // oneAmount
+            // 
+            this.oneAmount.Location = new System.Drawing.Point(540, 222);
+            this.oneAmount.Name = "oneAmount";
+            this.oneAmount.Size = new System.Drawing.Size(70, 20);
+            this.oneAmount.TabIndex = 20;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(616, 219);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 23);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "$1";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // dollarsRadio
+            // 
+            this.dollarsRadio.Location = new System.Drawing.Point(196, 318);
+            this.dollarsRadio.Name = "dollarsRadio";
+            this.dollarsRadio.Size = new System.Drawing.Size(104, 24);
+            this.dollarsRadio.TabIndex = 25;
+            this.dollarsRadio.TabStop = true;
+            this.dollarsRadio.Text = "Dollars";
+            this.dollarsRadio.UseVisualStyleBackColor = true;
+            this.dollarsRadio.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label11
+            // 
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(228, 129);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(100, 23);
+            this.label11.TabIndex = 26;
+            this.label11.Text = "Pounds";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label12
+            // 
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(228, 219);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(100, 23);
+            this.label12.TabIndex = 28;
+            this.label12.Text = "Yen";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label13
+            // 
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(228, 174);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(100, 23);
+            this.label13.TabIndex = 27;
+            this.label13.Text = "Euros";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // poundsRadio
+            // 
+            this.poundsRadio.Location = new System.Drawing.Point(306, 318);
+            this.poundsRadio.Name = "poundsRadio";
+            this.poundsRadio.Size = new System.Drawing.Size(104, 24);
+            this.poundsRadio.TabIndex = 29;
+            this.poundsRadio.TabStop = true;
+            this.poundsRadio.Text = "Pounds";
+            this.poundsRadio.UseVisualStyleBackColor = true;
+            this.poundsRadio.CheckedChanged += new System.EventHandler(this.poundsRadio_CheckedChanged);
+            // 
+            // eurosRadio
+            // 
+            this.eurosRadio.Location = new System.Drawing.Point(416, 318);
+            this.eurosRadio.Name = "eurosRadio";
+            this.eurosRadio.Size = new System.Drawing.Size(104, 24);
+            this.eurosRadio.TabIndex = 30;
+            this.eurosRadio.TabStop = true;
+            this.eurosRadio.Text = "Euros";
+            this.eurosRadio.UseVisualStyleBackColor = true;
+            this.eurosRadio.CheckedChanged += new System.EventHandler(this.eurosRadio_CheckedChanged);
+            // 
+            // yenRadio
+            // 
+            this.yenRadio.Location = new System.Drawing.Point(526, 318);
+            this.yenRadio.Name = "yenRadio";
+            this.yenRadio.Size = new System.Drawing.Size(104, 24);
+            this.yenRadio.TabIndex = 31;
+            this.yenRadio.TabStop = true;
+            this.yenRadio.Text = "Yen";
+            this.yenRadio.UseVisualStyleBackColor = true;
+            this.yenRadio.CheckedChanged += new System.EventHandler(this.yenRadio_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.yenRadio);
+            this.Controls.Add(this.eurosRadio);
+            this.Controls.Add(this.poundsRadio);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.dollarsRadio);
+            this.Controls.Add(this.oneAmount);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.fiveAmount);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tenAmount);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.twentyAmount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.fiftyAmount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.hundredAmmount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dollarsLabel);
+            this.Controls.Add(this.yenResult);
+            this.Controls.Add(this.eurosResult);
+            this.Controls.Add(this.poundsResult);
+            this.Controls.Add(this.dollarResult);
+            this.Controls.Add(this.convertButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.TextBox oneAmount;
+
+        private System.Windows.Forms.RadioButton poundsRadio;
+        private System.Windows.Forms.RadioButton eurosRadio;
+        private System.Windows.Forms.RadioButton yenRadio;
+
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+
+        private System.Windows.Forms.RadioButton dollarsRadio;
+
+        private System.Windows.Forms.TextBox hundredAmmount;
+        private System.Windows.Forms.TextBox fiftyAmount;
+        private System.Windows.Forms.TextBox twentyAmount;
+        private System.Windows.Forms.TextBox tenAmount;
+        private System.Windows.Forms.TextBox fiveAmount;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.TextBox dollarResult;
+        private System.Windows.Forms.TextBox poundsResult;
+        private System.Windows.Forms.TextBox eurosResult;
+        private System.Windows.Forms.TextBox yenResult;
+        private System.Windows.Forms.Label dollarsLabel;
+
+        private System.Windows.Forms.Button convertButton;
 
         #endregion
     }
--- currency-converter/Form1.cs
+++ currency-converter/Form1.cs
@@ -2,19 +2,161 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace currency_converter
 {
+    //a currency converter which takes the given values and converts it to whatever currency the user decides
+    // can convert to dollars, pounds, euros, and yen
     public partial class Form1 : Form
     {
+        //a dictionary of every currency type and its respective value
+        private readonly Dictionary<string, float> _currencyTypes = new Dictionary<string, float>()
+        {
+            {"dollar", 1f},
+            {"pound", 0.76f},
+            {"euro", 0.84f},
+            {"yen", 104.53f}
+        };
+        
+        private bool _dollarsChecked = false;
+        private bool _poundsChecked = false;
+        private bool _eurosChecked = false;
+        private bool _yenChecked = false;
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //gets all values from text boxes and turns them into integers
+        //compiles it into a total and then called converting process to get a final amount for every currency type
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            var hundreds = 0f;
+            var fiftys = 0f;
+            var twentys = 0f;
+            var tens = 0f;
+            var fives = 0f;
+            var ones = 0f;
+            if(!String.IsNullOrEmpty(hundredAmmount.Text))  hundreds = Int32.Parse(hundredAmmount.Text) * 100f;
+            if(!String.IsNullOrEmpty(fiftyAmount.Text))  fiftys = Int32.Parse(fiftyAmount.Text) * 50f;
+            if(!String.IsNullOrEmpty(twentyAmount.Text))  twentys = Int32.Parse(twentyAmount.Text) * 20f;
+            if(!String.IsNullOrEmpty(tenAmount.Text))  tens = Int32.Parse(tenAmount.Text) * 10f;
+            if(!String.IsNullOrEmpty(fiveAmount.Text))  fives = Int32.Parse(fiveAmount.Text) * 5f;
+            if(!String.IsNullOrEmpty(oneAmount.Text) )  ones = Int32.Parse(oneAmount.Text) * 1f;
+
+
+            var moneyTotal = hundreds + fiftys + twentys + tens + fives + ones;
+            
+            if(_dollarsChecked)  convert("dollar", moneyTotal);
+            if(_poundsChecked)  convert("pound", moneyTotal);
+            if(_eurosChecked)  convert("euro", moneyTotal);
+            if(_yenChecked) convert("yen", moneyTotal);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        //begins converting by finding out what were converting to
+        private void convert(string currencyType, float totalMoney)
+        {
+            foreach (var currency in _currencyTypes)
+            {
+                if (currencyType == currency.Key)
+                {
+                    convertValues(currency.Key, currency.Value, totalMoney);
+                }
+            }
+        }
+
+        //converts the given values into final results for every currency type
+        private void convertValues(string currencyName, float currencyValue, float totalMoney)
+        {
+            var dollarResultVal = 0f;
+            var poundResult = 0f;
+            var euroResult = 0f;
+            var yenResultVal = 0f;
+            //total times conversion
+            foreach (var conversion in _currencyTypes)
+                //convert to dollars then convert to other values
+            {
+                switch (conversion.Key)
+                {
+                    case "dollar":
+                        dollarResultVal = totalMoney * currencyValue * conversion.Value;
+
+                        break;
+                    case "pound":
+                        poundResult = totalMoney * currencyValue * conversion.Value;
+
+                        break;
+                    case "euro":
+                        euroResult = totalMoney * currencyValue * conversion.Value;
+
+                        break;
+                    case "yen":
+                        yenResultVal = totalMoney * currencyValue * conversion.Value;
+
+                        break;
+                }
+
+                this.dollarResult.Text = dollarResultVal.ToString();
+                this.poundsResult.Text = poundResult.ToString();
+                this.eurosResult.Text = euroResult.ToString();
+                this.yenResult.Text = yenResultVal.ToString();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        //when a radio button is changed, finds out which one is checked and makes all others false
+        private void checkConvert()
+        {
+            _dollarsChecked = false;
+            _poundsChecked = false;
+            _eurosChecked = false;
+            _yenChecked = false;
+            
+            if (dollarsRadio.Checked) _dollarsChecked = true;
+            if (poundsRadio.Checked) _poundsChecked = true;
+            if (eurosRadio.Checked) _eurosChecked = true;
+            if (yenRadio.Checked) _yenChecked = true;
+        }
+        
+        //listens to a change in all radio buttons
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            checkConvert();
+        }
+
+        private void poundsRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            checkConvert();
+        }
+
+        private void eurosRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            checkConvert();
+        }
+
+        private void yenRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            checkConvert();
+        }
+
+        private void dollarResult_TextChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
